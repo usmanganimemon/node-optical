c9eaa61a26e4ce6610132e108621b7c37ea0d796
Usman Memon <usmanganimemon45@gmail.com>
2022-05-23 19:20:28 +0530

    DB name set

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize-typescript'
-const sequelize = new Sequelize('practice', 'root', 'root@123', {
+const sequelize = new Sequelize('testDB', 'root', 'root@123', {
   host: 'localhost',
   dialect: 'mysql'
 })
