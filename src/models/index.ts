06333df45ad36e997468a404cd3333b7b1283d6d
Usman Memon <usmanganimemon45@gmail.com>
2022-05-25 12:42:37 +0530

    Password changes

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize-typescript'
-const sequelize = new Sequelize('testDB', 'root', 'root@123', {
+const sequelize = new Sequelize('testDB', 'root', 'Jabu@1994', {
   host: 'localhost',
   dialect: 'mysql'
 })
